a7ebb54f0f9565cc616ced5bd5ad1b8ef51a42ee
Naman Raichura <naman.raichura@gmail.com>
2026-08-10 12:36:31 -0700

    First notes initalized about Java syntax
    Day 1

---
+++ unit1.java
@@ -0,0 +1,15 @@
+public class unit1 {
+    public static void main(String[] args)
+    {
+        System.out.prinln(x:"Git Hub is working!!!")
+    }
+}
+// Public - 
+// Static - method doesn't belong to object; belongs to class
+    // Objects: copy of class
+// Void - no return statement
+// --> HAS TO MEAN main()
+
+
+
+
