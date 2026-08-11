5e34971678d49e0767676e1902168398ae4f2cb7
Naman Raichura <naman.raichura@gmail.com>
2026-08-11 11:50:33 -0700

    Updated day 1 entirely

--- unit1.java
+++ unit1.java
@@ -1,15 +1,29 @@
-public class unit1 {
+
+
+public class unit1 { // has to match progam files
     public static void main(String[] args)
     {
-        System.out.prinln(x:"Git Hub is working!!!")
-    }
+        // text- string literal
+        System.out.print("Github is working!"); // prints on the same line
+        System.out.println("Git Hub is working!!!"); // semi colon on everything except class definitions
+        int age; // Default of 0 
+        age = 50;
+        age=49;
+        System.out.println("I am "+ age + "years old.");
+        double weight; // int cannot be a decimal
+        weight = 50.0;
+        System.out.print("I weight "+ weight +" pounds");
+        double height = 5.0; //double can be an integer or decimal
+        double weight = 50.0;
+        
+    }// class           x: place holder:   // println is a method - defined in class systems 
 }
-// Public - 
+// Public - allows for access to classes
 // Static - method doesn't belong to object; belongs to class
     // Objects: copy of class
 // Void - no return statement
-// --> HAS TO MEAN main()
+// --> HAS TO MEAN main
 
+// Python is efficient in processing and code, but less memory efficent - versus java
 
-
-
+// 
